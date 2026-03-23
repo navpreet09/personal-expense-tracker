ae72b06ead3739ad34496d08dec5a58f5637195c
navpreet <gillnavpreetkaur8@gmail.com>
2026-03-23 01:24:32 -0400

    add email service integration for sending summaries

---
+++ email_service.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
+
+class EmailService:
+
+    def send_summary(self, email, summary_text):
+
+        message = Mail(
+            from_email='gillnavpreetkaur8@gmail.com',
+            to_emails=email,
+            subject='Expense Summary',
+            plain_text_content=summary_text
+        )
+
+        try:
+            sg = SendGridAPIClient(os.getenv("SENDGRID_API_KEY"))
+            response = sg.send(message)
+            print("Email sent. Status code:", response.status_code)
+
+        except Exception as e:
+            print(e)
