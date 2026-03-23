cedc03a3e7d97f609c4411fedae6b53ad3b59d61
navpreet <gillnavpreetkaur8@gmail.com>
2026-03-23 01:07:56 -0400

    implement add expense functionality in CLI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from repository import ExpenseRepository
+from models import Expense
 
 repo = ExpenseRepository()
 
@@ -9,3 +10,13 @@ print("4. Filter expenses by category")
 print("5. Send monthly summary")
 
 choice = input("Choose an option: ")
+
+if choice == "1":
+    amount = float(input("Enter amount: "))
+    category = input("Enter category: ")
+    date = input("Enter date (YYYY-MM-DD): ")
+
+    expense = Expense(amount, category, date)
+    repo.add_expense(expense)
+
+    print("Expense added successfully.")
