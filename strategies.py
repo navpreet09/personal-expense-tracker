5e27509da1c51714f88f469593443e0f1dd6ae0c
navpreet <gillnavpreetkaur8@gmail.com>
2026-03-23 01:17:36 -0400

    add monthly summary strategy implementation

---
+++ strategies.py
@@ -0,0 +1,8 @@
+class MonthlySummaryStrategy:
+
+    def generate_summary(self, expenses):
+        total = 0
+        for expense in expenses:
+            total += expense.amount
+
+        return f"Monthly total spending: {total}"
