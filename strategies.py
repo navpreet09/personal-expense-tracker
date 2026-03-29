104c629a09635e9282430b0abf99e31f2d05ca43
navpreet <gillnavpreetkaur8@gmail.com>
2026-03-28 20:27:25 -0400

    Improve Strategy Pattern with multiple strategies and runtime selection

--- strategies.py
+++ strategies.py
@@ -1,3 +1,7 @@
+class SummaryStrategy:
+    def generate_summary(self, expenses):
+        pass
+
 class MonthlySummaryStrategy:
 
     def generate_summary(self, expenses):
@@ -6,3 +10,12 @@ class MonthlySummaryStrategy:
             total += expense.amount
 
         return f"Monthly total spending: {total}"
+    
+class TotalSummaryStrategy(SummaryStrategy):
+    def generate_summary(self, expenses):
+        total = 0
+        for expense in expenses:
+            total += expense.amount
+
+        return f"Total spending: {total}"
+
